8edf278b38721661a2cadd4852586fda99951858
Vineet Jakhar <vineet.iitdelhi@gmail.com>
2023-12-21 02:12:44 +0530

    Merge remote changes into local branch

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const path = require('path');
+// const path = require('path');
 const { Server } = require('socket.io');
 const ACTIONS = require('./src/Actions');
 
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('build'));
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.use(express.static('build'));
+// app.use((req, res, next) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 const userSocketMap = {};
-function getAllConnectedClients(roomId) {
-    // Map
+const getAllConnectedClients = (roomId) => {
     return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
         (socketId) => {
             return {
@@ -24,7 +23,7 @@ function getAllConnectedClients(roomId) {
             };
         }
     );
-}
+};
 
 io.on('connection', (socket) => {
     console.log('socket connected', socket.id);
